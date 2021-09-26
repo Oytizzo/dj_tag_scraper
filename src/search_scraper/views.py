d673d4d0075786c3bb607b3057703cf10c26747f
Oytizzo <oytizzo@gmail.com>
2021-09-26 14:21:49 +0600

    Add html(s) and erase unused code

--- src/search_scraper/views.py
+++ src/search_scraper/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 from .tasks import add
 
 
 def hello(request):
     print("Hello")
-    add.delay(100, 9, 10)
-    return HttpResponse("<h1>Hello world!</h1>")
+    add.delay(100, 111, 10)
+    return render(request, 'search_scraper/hello.html', {})
