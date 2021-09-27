11ffe2687018679aff5a7ce37734dc772aefe7ba
Oytizzo <oytizzo@gmail.com>
2021-09-27 16:04:50 +0600

    Add models and views with html

--- src/search_scraper/views.py
+++ src/search_scraper/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
 
 from .tasks import add
+from .models import SearchItem
 
 
 def hello(request):
     print("Hello")
     add.delay(100, 111, 10)
     return render(request, 'search_scraper/hello.html', {})
+
+
+class SearchListView(generic.ListView):
+    model = SearchItem
+    template_name = 'search_scraper/search_list.html'
