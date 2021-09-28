11a1120da3476c3ab3b8457650cb96057a5b8a0d
Oytizzo <oytizzo@gmail.com>
2021-09-28 23:52:13 +0600

    Install requests and add basic scraper

---
+++ src/search_scraper/scrapers.py
@@ -0,0 +1,7 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape_dev_dot_to(url):
+    pass
--- src/search_scraper/tasks.py
+++ src/search_scraper/tasks.py
@@ -1,10 +1,17 @@
-# Create your tasks here
 from time import sleep
-
 from celery import shared_task
+
+from .scrapers import scrape_dev_dot_to
 
 
 @shared_task
 def add(x, y, sec):
     sleep(sec)
     return x + y
+
+
+@shared_task
+def task_scrape_dev_dot_to():
+    url = ""
+    scrape_dev_dot_to(url)
+    return
